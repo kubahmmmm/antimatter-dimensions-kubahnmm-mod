798ecdfbb7c84da182685c5397e7d22690aa8171
Kuba <131533917+kubahmmmm@users.noreply.github.com>
2025-07-01 15:08:20 +0200

    Update dusting.vue

--- src/components/tabs/dust/dusting.vue
+++ src/components/tabs/dust/dusting.vue
@@ -1,264 +1,164 @@
 <script>
-import "vue-loading-overlay/dist/vue-loading.css";
-
-import Loading from "vue-loading-overlay";
-
-import Payments from "@/core/payments";
-
-import { STEAM } from "@/env";
-import { SteamRuntime } from "@/steam";
-
+import AntimatterDimensionProgressBar from "./AntimatterDimensionProgressBar";
+import AntimatterDimensionRow from "@/components/tabs/antimatter-dimensions/ModernAntimatterDimensionRow";
+import AntimatterGalaxyRow from "@/components/tabs/antimatter-dimensions/ModernAntimatterGalaxyRow";
+import DimensionBoostRow from "@/components/tabs/antimatter-dimensions/ModernDimensionBoostRow";
 import PrimaryButton from "@/components/PrimaryButton";
-import ShopButton from "./ShopButton";
+import TickspeedRow from "@/components/tabs/antimatter-dimensions/TickspeedRow";
 
 export default {
-  name: "ShopTab",
+  name: "ModernAntimatterDimensionsTab",
   components: {
-    ShopButton,
-    Loading,
     PrimaryButton,
+    AntimatterDimensionProgressBar,
+    AntimatterDimensionRow,
+    AntimatterGalaxyRow,
+    DimensionBoostRow,
+    TickspeedRow
   },
   data() {
     return {
-      availableSTD: 0,
-      spentSTD: 0,
-      isLoading: false,
-      IAPsEnabled: false,
-      creditsClosed: false,
-      loggedIn: false,
-      username: "",
-      canRespec: false,
-      respecTimeStr: "",
+      hasDimensionBoosts: false,
+      buyUntil10: true,
+      isSacrificeUnlocked: false,
+      isSacrificeAffordable: false,
+      buy10Mult: new Decimal(0),
+      currentSacrifice: new Decimal(0),
+      sacrificeBoost: new Decimal(0),
+      disabledCondition: "",
+      isQuickResetAvailable: false,
+      hasContinuum: false,
+      isContinuumActive: false,
+      multiplierText: "",
+      isFullyAutomated: false,
     };
   },
   computed: {
-    STEAM() {
-      return STEAM;
+    sacrificeTooltip() {
+      if (this.isFullyAutomated) {
+        return "Sacrifice autobuyer is enabled and Achievement 118 is unlocked, so Sacrifice is now fully automated";
+      }
+      return `Boosts 8th Antimatter Dimension by ${formatX(this.sacrificeBoost, 2, 2)}`;
     },
-    purchases() {
-      return ShopPurchase.all;
-    },
-    enableText() {
-      return `In-app Purchases: ${this.IAPsEnabled ? "Enabled" : "Disabled"}`;
-    },
-    respecText() {
-      if (!this.loggedIn) return "Not logged in!";
-      if (!this.canRespec) return "No respec available! (Purchase STDs or wait 3 days since your last one)";
-      return null;
-    },
-    hiddenName() {
-      return player.options.hideGoogleName;
-    }
   },
   methods: {
-    update() {
-      this.availableSTD = ShopPurchaseData.availableSTD;
-      this.spentSTD = ShopPurchaseData.spentSTD;
-      this.isLoading = Boolean(player.IAP.checkoutSession.id);
-      this.IAPsEnabled = player.IAP.enabled;
-      this.creditsClosed = GameEnd.creditsEverClosed;
-      this.loggedIn = Cloud.loggedIn;
-      this.username = Cloud.user?.displayName;
-      this.canRespec = ShopPurchaseData.canRespec;
-      if (!ShopPurchaseData.respecAvailable && !this.canRespec) {
-        this.respecTimeStr = ShopPurchaseData.timeUntilRespec.toStringShort();
+    maxAll() {
+      maxAll();
+    },
+    sacrifice() {
+      sacrificeBtnClick();
+    },
+    // Toggle single/10 without Continuum, otherwise cycle through all 3 if it's unlocked
+    changeBuyMode() {
+      if (!this.hasContinuum) {
+        player.buyUntil10 = !player.buyUntil10;
+        return;
+      }
+      // "Continuum" => "Until 10" => "Buy 1" => "Continuum"
+      if (this.isContinuumActive) {
+        Laitela.setContinuum(false);
+        player.buyUntil10 = true;
+      } else if (player.buyUntil10) {
+        player.buyUntil10 = false;
+      } else {
+        if (ImaginaryUpgrade(21).isLockingMechanics && player.auto.disableContinuum) {
+          ImaginaryUpgrade(21).tryShowWarningModal();
+          return;
+        }
+        Laitela.setContinuum(true);
       }
     },
-    showStore() {
-      if (STEAM && !SteamRuntime.isActive) return;
-      if (this.creditsClosed) return;
-      SecretAchievement(33).unlock();
-      if (this.loggedIn) Modal.shop.show();
-      else Modal.message.show("You cannot purchase STD coins without logging in first.");
+    getUntil10Display() {
+      if (this.isContinuumActive) return "Continuum";
+      return this.buyUntil10 ? "Until 10" : "Buy 1";
     },
-    onCancel() {
-      Payments.cancelPurchase(false);
-    },
-    respec() {
-      if (this.creditsClosed || !this.loggedIn || !this.canRespec) return;
-      ShopPurchaseData.respecRequest();
-    },
-    toggleEnable() {
-      if (ShopPurchaseData.availableSTD < 0) return;
-      player.IAP.enabled = !player.IAP.enabled;
-      if (ShopPurchaseData.isIAPEnabled) Speedrun.setSTDUse(true);
-    },
-    respecClass() {
-      return {
-        "o-primary-btn--subtab-option": true,
-        "o-pelle-disabled-pointer": this.creditsClosed,
-        "o-primary-btn--disabled": !this.loggedIn || !this.canRespec
-      };
+    update() {
+      this.hasDimensionBoosts = player.dimensionBoosts > 0;
+      this.buyUntil10 = player.buyUntil10;
+      this.hasContinuum = Laitela.continuumUnlocked;
+      this.isContinuumActive = Laitela.continuumActive;
+      this.isQuickResetAvailable = Player.isInAntimatterChallenge && Player.antimatterChallenge.isQuickResettable;
+
+      const isSacrificeUnlocked = Sacrifice.isVisible;
+      this.isSacrificeUnlocked = isSacrificeUnlocked;
+
+      this.buy10Mult.copyFrom(AntimatterDimensions.buyTenMultiplier);
+
+      this.multiplierText = `Buy 10 Dimension purchase multiplier: ${formatX(this.buy10Mult, 2, 2)}`;
+      if (!isSacrificeUnlocked) return;
+      this.isFullyAutomated = Autobuyer.sacrifice.isActive && Achievement(118).isUnlocked;
+      this.isSacrificeAffordable = Sacrifice.canSacrifice && !this.isFullyAutomated;
+      this.currentSacrifice.copyFrom(Sacrifice.totalBoost);
+      this.sacrificeBoost.copyFrom(Sacrifice.nextBoost);
+      this.disabledCondition = Sacrifice.disabledCondition;
+      const sacText = this.isSacrificeUnlocked
+        ? ` | Dimensional Sacrifice multiplier: ${formatX(this.currentSacrifice, 2, 2)}`
+        : "";
+      this.multiplierText += sacText;
     }
-  },
+  }
 };
 </script>
 
 <template>
-  <div class="tab shop">
-    <div class="c-shop-disclaimer">
-      Disclaimer: These are not required to progress in the game, they are just for supporting the developer.
-      The game is balanced without the use of any microtransactions.
-    </div>
-    <div>
-      Note: Shop purchases made on the Android, Steam, and Web versions are
-      separate and non-transferable due to legal reasons.
-    </div>
-    <div class="c-subtab-option-container">
-      <PrimaryButton
-        class="o-primary-btn--subtab-option"
-        :class="{ 'o-pelle-disabled-pointer': creditsClosed }"
-        label="Disable in-app-purchases:"
-        @click="toggleEnable()"
-      >
-        {{ enableText }}
-      </PrimaryButton>
-      <PrimaryButton
-        v-if="!STEAM"
-        v-tooltip="respecText"
-        :class="respecClass()"
-        @click="respec()"
-      >
-        Respec Shop
-      </PrimaryButton>
-    </div>
-    <div v-if="loggedIn && !canRespec && !STEAM">
-      Time until respec available: {{ respecTimeStr }}
-    </div>
-    <div
-      v-if="loggedIn"
-      class="c-login-info"
-    >
-      <template v-if="STEAM">
-        You are logged in as {{ username }}.
-      </template>
-      <template v-else>
-        <span v-if="hiddenName">You are logged in. <i>(name hidden)</i></span>
-        <span v-else>You are logged in as {{ username }}.</span>
-        <button
-          class="o-shop-button-button"
-          onclick="GameOptions.logout()"
-        >
-          Disconnect Google Account
-        </button>
-      </template>
-    </div>
-    <div
-      v-else
-      class="c-login-info"
-    >
-      You must be logged in to purchase STD coins or use these upgrades.
+  <div class="l-antimatter-dim-tab">
+    <div class="modes-container">
       <button
-        class="o-shop-button-button"
-        onclick="GameOptions.login()"
+        class="o-primary-btn l-button-container"
+        @click="changeBuyMode"
       >
-        Login with Google
+        {{ getUntil10Display() }}
+      </button>
+      <PrimaryButton
+        v-show="isSacrificeUnlocked"
+        v-tooltip="sacrificeTooltip"
+        :enabled="isSacrificeAffordable"
+        class="o-primary-btn--sacrifice"
+        @click="sacrifice"
+      >
+        <span v-if="isSacrificeAffordable">Dimensional Sacrifice ({{ formatX(sacrificeBoost, 2, 2) }})</span>
+        <span v-else-if="isFullyAutomated && disabledCondition !== ''">
+          Dimensional Sacrifice is Automated (Achievement 118)
+        </span>
+        <span v-else>Dimensional Sacrifice Disabled ({{ disabledCondition }})</span>
+      </PrimaryButton>
+      <button
+        class="o-primary-btn l-button-container"
+        @click="maxAll"
+      >
+        Max All (M)
       </button>
     </div>
-    <div class="c-shop-header">
-      <span>You have {{ availableSTD }}</span>
-      <img
-        src="images/std_coin.png"
-        class="c-shop-header__img"
-      >
-      <button
-        class="o-shop-button-button"
-        :class="{ 'o-shop-button-button--disabled': !loggedIn }"
-        @click="showStore()"
-      >
-        Buy More
-      </button>
-    </div>
-    Note: All numbers on this page are intentionally unaffected by your notation settings
-    <div class="l-shop-buttons-container">
-      <ShopButton
-        v-for="purchase in purchases"
-        :key="purchase.key"
-        :purchase="purchase"
+    <span>{{ multiplierText }}</span>
+    <TickspeedRow />
+    <div class="l-dimensions-container">
+      <AntimatterDimensionRow
+        v-for="tier in 8"
+        :key="tier"
+        :tier="tier"
       />
     </div>
-    <loading
-      :active="isLoading"
-      :can-cancel="true"
-      :on-cancel="onCancel"
-      :is-full-page="true"
-    />
+    <div class="resets-container">
+      <DimensionBoostRow />
+      <PrimaryButton
+        v-if="isQuickResetAvailable"
+        class="o-primary-btn--quick-reset"
+        onclick="softReset(-1, true, true)"
+      >
+        Perform a Dimension Boost reset
+        <span v-if="hasDimensionBoosts"> but lose a Dimension Boost</span>
+        <span v-else> for no gain</span>
+      </PrimaryButton>
+      <AntimatterGalaxyRow />
+    </div>
+    <AntimatterDimensionProgressBar />
   </div>
 </template>
 
 <style scoped>
-.shop {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  color: var(--color-text);
-}
-
-.c-shop-disclaimer {
-  width: 80%;
-  max-width: 100rem;
-  font-size: 1.8rem;
-  font-weight: bold;
-  color: black;
-  background: var(--color-bad);
-  border: var(--var-border-width, 0.2rem) solid black;
-  border-radius: var(--var-border-radius, 1rem);
-  margin-top: 0.8rem;
-}
-
-.t-s1 .c-shop-disclaimer,
-.t-s6 .c-shop-disclaimer,
-.t-s10 .c-shop-disclaimer {
-  color: var(--color-bad);
-  background: black;
-  border-color: var(--color-bad);
-}
-
-.c-login-info {
-  font-size: 1.5rem;
-}
-
-.c-shop-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
-  margin: 1rem 0;
-}
-
-.c-shop-header__img {
-  height: 40px;
-  margin: 0 1rem;
-}
-
-.o-shop-button-button {
-  display: flex;
-  align-items: center;
-  font-family: Typewriter;
-  background: turquoise;
-  border: none;
-  border-radius: var(--var-border-radius, 0.5rem);
-  margin: auto;
-  margin-top: 1rem;
-  padding: 0.5rem 2rem;
-  cursor: pointer;
-}
-
-.o-shop-button-button--disabled {
-  background: rgb(150, 150, 150);
-  cursor: default;
-}
-
-.l-shop-buttons-container {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  width: 93rem;
-  margin: auto;
-}
-
-.c-shop-header .o-shop-button-button {
-  margin: 0;
+.l-button-container {
+  width: 100px;
+  height: 30px;
+  padding: 0;
 }
 </style>
